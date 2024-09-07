c6f13c6656c372d2ecbae08f37c8462031f680d2
Dieguidev <diegogaraycullas@gmail.com>
2024-09-07 08:29:35 -0500

    Refactorizar OrderSidebar para obtener categorías desde la base de datos
    Se crea prisma.ts para solo crear una instancia de prisma

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,7 +1,14 @@
+import { prisma } from "@/src/lib/prisma";
 
 
-export default function OrderSidebar() {
-  return (
-    <div className="md:w-72 md:h-screen bg-white">OrderSidebar</div>
-  )
+async function getCategories() {
+  const categories = await prisma.category.findMany();
+  return categories;
+}
+
+export default async function OrderSidebar() {
+  const categories = await getCategories();
+  console.log(categories);
+
+  return <div className="md:w-72 md:h-screen bg-white">OrderSidebar</div>;
 }
