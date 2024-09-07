622442d2a28449dace216088bf7bc3f229b632af
Dieguidev <diegogaraycullas@gmail.com>
2024-09-07 14:22:29 -0500

    Se intala zustand y se crea el store

--- components/ui/CategoryIcon.tsx
+++ components/ui/CategoryIcon.tsx
@@ -9,11 +9,13 @@ type CategoryIconProps = {
 };
 
 export default function CategoryIcon({ category }: CategoryIconProps) {
-  const params = useParams<{category: string}>();
+  const params = useParams<{ category: string }>();
 
   return (
     <div
-      className={`${category.slug === params.category ? 'bg-amber-400': ''} flex items-center gap-4 border-t border-gray-200 p-3 last-of-type:border-b`}
+      className={`${
+        category.slug === params.category ? "bg-amber-400" : ""
+      } flex items-center gap-4 border-t border-gray-200 p-3 last-of-type:border-b`}
     >
       <div className="w-16 h-16 relative">
         <Image fill src={`/icon_${category.slug}.svg`} alt={category.name} />
