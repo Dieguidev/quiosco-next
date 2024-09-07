b41915709ae3612f9695425484117299c8029faf
Dieguidev <diegogaraycullas@gmail.com>
2024-09-07 08:51:38 -0500

    Refactorizar CategoryIcon para mostrar el nombre de la categoría y agregar un ícono

--- components/ui/CategoryIcon.tsx
+++ components/ui/CategoryIcon.tsx
@@ -1,11 +1,19 @@
-import { Category } from "@prisma/client"
+import { Category } from "@prisma/client";
+import Image from "next/image";
 
 type CategoryIconProps = {
   category: Category;
-}
+};
 
 export default function CategoryIcon({ category }: CategoryIconProps) {
   return (
-    <div>{category.name}</div>
-  )
+    <div
+      className={`flex items-center gap-4 border-t border-gray-200 p-3 last-of-type:border-b`}
+    >
+      <div className="w-16 h-16 relative">
+        <Image fill src={`/icon_${category.slug}.svg`} alt={category.name} />
+      </div >
+      <p className="text-xl font-bold">{category.name}</p>
+    </div>
+  );
 }
