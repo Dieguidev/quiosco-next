15e21925bc98f93af6d022c41efc18009396fcaa
Dieguidev <diegogaraycullas@gmail.com>
2024-09-07 09:43:49 -0500

    Refactorizar OrderPage para mostrar productos de una categoría específica desde la base de datos y agregar título principal
    Finalizando los Card de Productos

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -22,11 +22,14 @@ export default async function OrderPage({
   const products= await getProducts(params.category);
 
   return (
+    <>
+    <h1 className="text-2xl my-10">Elige y personaliza tu pedido a continuación</h1>
     <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start">
       {products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
 
     </div>
+      </>
   );
 }
