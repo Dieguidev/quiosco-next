f675223568141ba38f1f642d2ba44ab4a42b17cc
Dieguidev <diegogaraycullas@gmail.com>
2024-09-08 13:41:29 -0500

    Refactorizar OrderSummary para mostrar el contenido del carrito de compra en la página de resumen del pedido y agregar funcionalidad para confirmar el pedido

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -16,16 +16,24 @@ export default function OrderSummary() {
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
       <h1 className="text-4xl text-center font-black">Mi Pedido</h1>
       {order.length === 0 ? (
-        <p className="text-center my-10">El carrito esta vacio</p>
+        <p className="text-center my-10">El pedido esta vacio</p>
       ) : (
         <div className="mt-5">
           {order.map((item) => (
             <ProductDetails key={item.id} item={item} />
           ))}
           <p className="text-2xl mt-20 text-center">
-            Total a pagar: {""}{" "}
+            Total a pagar: {""}
             <span className="font-bold">{formatCurrency(total)}</span>
           </p>
+
+          <form className="w-full mt-10 space-y-5" >
+            <input
+              type="submit"
+              className="py-2 rounded uppercase text-white bg-black w-full text-center cursor-pointer font-bold"
+              value='Confirmar pedido'
+            />
+          </form>
         </div>
       )}
     </aside>
