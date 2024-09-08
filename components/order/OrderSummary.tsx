1c5257132ec3918c2cccfe4194774b7a457f4bfd
Dieguidev <diegogaraycullas@gmail.com>
2024-09-08 18:58:42 -0500

    Refactorizar OrderSummary para agregar notificaciones de error al validar el formulario

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -5,6 +5,7 @@ import { useMemo } from "react";
 import { formatCurrency } from "@/src/utils";
 import { createOrder } from "@/actions/create-order-action";
 import { OrderSchema } from "@/src/schema";
+import { toast } from "react-toastify";
 
 export default function OrderSummary() {
   const order = useStore((state) => state.order);
@@ -20,7 +21,11 @@ export default function OrderSummary() {
     }
 
     const result = OrderSchema.safeParse(data);
-    console.log(result);
+    if (!result.success) {
+      result.error.issues.forEach(issue =>{
+        toast.error(issue.message)
+      })
+    }
 
     return
     createOrder();
