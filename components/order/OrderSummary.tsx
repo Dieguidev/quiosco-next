5eb48f4f5643fb94e4b02ac2f1b716aa6ec250c9
Dieguidev <diegogaraycullas@gmail.com>
2024-09-08 13:20:16 -0500

    Refactorizar OrderSummary para mostrar el contenido del carrito de compra y el total a pagar

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -1,9 +1,17 @@
-'use client'
+"use client";
 import { useStore } from "@/src/store";
 import ProductDetails from "./ProductDetails";
+import { useMemo } from "react";
+import { formatCurrency } from "@/src/utils";
 
 export default function OrderSummary() {
   const order = useStore((state) => state.order);
+
+  const total = useMemo(
+    () => order.reduce((total, item) => total + item.quantity * item.price, 0),
+    [order]
+  );
+
   return (
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
       <h1 className="text-4xl text-center font-black">Mi Pedido</h1>
@@ -11,11 +19,13 @@ export default function OrderSummary() {
         <p className="text-center my-10">El carrito esta vacio</p>
       ) : (
         <div className="mt-5">
-          {
-            order.map(item =>(
-              <ProductDetails key={item.id} item={item}/>
-            ))
-          }
+          {order.map((item) => (
+            <ProductDetails key={item.id} item={item} />
+          ))}
+          <p className="text-2xl mt-20 text-center">
+            Total a pagar: {""}{" "}
+            <span className="font-bold">{formatCurrency(total)}</span>
+          </p>
         </div>
       )}
     </aside>
