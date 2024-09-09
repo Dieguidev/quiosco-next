7e2b2077a6761686c7a52b89820c950954c03f27
Dieguidev <diegogaraycullas@gmail.com>
2024-09-08 19:49:06 -0500

    Refactorizar OrderSidebar para agregar el logotipo de la empresa

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,5 +1,6 @@
 import { prisma } from "@/src/lib/prisma";
 import CategoryIcon from "../ui/CategoryIcon";
+import Logo from "../ui/Logo";
 
 async function getCategories() {
   const categories = await prisma.category.findMany();
@@ -11,9 +12,10 @@ export default async function OrderSidebar() {
 
   return (
     <aside className="md:w-72 md:h-screen bg-white">
+      <Logo />
       <nav className="mt-10">
         {categories.map((category) => (
-          <CategoryIcon key={category.id} category={category}/>
+          <CategoryIcon key={category.id} category={category} />
         ))}
       </nav>
     </aside>
