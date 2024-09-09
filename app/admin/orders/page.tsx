abdacc2a462b454cf110fccad75f65cce8639f2a
Dieguidev <diegogaraycullas@gmail.com>
2024-09-08 21:02:45 -0500

    Refactorizar página de administración de órdenes y agregar funcionalidad para obtener órdenes pendientes desde la base de datos

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,10 +1,29 @@
 import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
 
+async function getPendingOrders() {
+  const orders = await prisma.order.findMany({
+    where: {
+      status: false,
+    },
+    include: {
+      orderProducts: {
+        include: {
+          product: true,
+        },
+      },
+    },
+  });
+  return orders;
+}
 
-export default function page() {
+export default async function page() {
+  const orders = await getPendingOrders();
+  console.log(orders);
+
   return (
     <>
-    <Heading>Administrar Ordenes</Heading>
+      <Heading>Administrar Ordenes</Heading>
     </>
-  )
+  );
 }
