c3aab9018503b5b2c2b00729d84fd9fb5bf0fbba
Dieguidev <diegogaraycullas@gmail.com>
2024-09-09 18:44:30 -0500

    Refactorizar página de administración de productos para incluir paginación en la tabla de productos existentes

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -2,8 +2,12 @@ import ProductTable from "@/components/products/ProductTable";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
 
-async function getProducts() {
+async function getProducts(page: number, pageSize: number) {
+  const skip = (page - 1) * pageSize;
+
   const products = await prisma.product.findMany({
+    take: pageSize,
+    skip,
     include: { category: true },
   });
 
@@ -13,8 +17,15 @@ async function getProducts() {
 // esto es un type que infiere el tipo de retorno de la funcion getProducts
 export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>>;
 
-export default async function ProductsPage() {
-  const products = await getProducts();
+export default async function ProductsPage({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
+  const page = +searchParams.page || 1;
+  const pageSize = 10;
+
+  const products = await getProducts(page, pageSize);
 
   return (
     <>
