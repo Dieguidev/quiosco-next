b6e28a14b75d0e004037691aa279db6a8e5675a2
Dieguidev <diegogaraycullas@gmail.com>
2024-09-09 18:34:49 -0500

    Refactorizar página de administración de productos para incluir categorías en la tabla de productos existentes
    Refactorizar componente ProductTable para utilizar el tipo ProductsWithCategory y mostrar el nombre de la categoría en lugar del ID

--- components/products/ProductTable.tsx
+++ components/products/ProductTable.tsx
@@ -1,9 +1,9 @@
-import { formatCurrency } from "@/src/utils";
-import { Product } from "@prisma/client";
 import Link from "next/link";
+import { ProductsWithCategory } from "@/app/admin/products/page";
+import { formatCurrency } from "@/src/utils";
 
 type ProcudtTebleProps = {
-  products: Product[];
+  products: ProductsWithCategory;
 };
 
 export default function ProductTable({ products }: ProcudtTebleProps) {
@@ -48,13 +48,15 @@ export default function ProductTable({ products }: ProcudtTebleProps) {
                       {formatCurrency(product.price)}
                     </td>
                     <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                      {product.categoryId}
+                      {product.category.name}
                     </td>
                     <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
                       <Link
                         href={`/admin/products/${product.id}/edit`}
                         className="text-indigo-600 hover:text-indigo-800"
-                      >Editar <span className="sr-only">, {product.name}</span></Link>
+                      >
+                        Editar <span className="sr-only">, {product.name}</span>
+                      </Link>
                     </td>
                   </tr>
                 ))}
