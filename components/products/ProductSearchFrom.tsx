04e0ed7c58d43eee9c86c426ccb7f7dd9abdaa01
Dieguidev <diegogaraycullas@gmail.com>
2024-09-10 18:45:01 -0500

    Refactorizar formulario de búsqueda de productos en la página de administración
    Se refactorizó el componente ProductSearchFrom en la página de administración de productos para incluir un formulario de búsqueda de productos. Ahora, al ingresar un término de búsqueda y hacer clic en el botón "Buscar", se redirige a la página de resultados de búsqueda correspondiente. Además, se agregó validación para asegurarse de que el campo de búsqueda no esté vacío.
    
    Refs: #123

--- components/products/ProductSearchFrom.tsx
+++ components/products/ProductSearchFrom.tsx
@@ -1,15 +1,36 @@
-import React from "react";
+"use client";
+import { SearchSchema } from "@/src/schema";
+import { redirect } from "next/navigation";
+import { toast } from "react-toastify";
 
 export default function ProductSearchFrom() {
+  const handleSearchForm = (formData: FormData) => {
+    const data = {
+      search: formData.get("search"),
+    };
+    const result = SearchSchema.safeParse(data);
+    if (!result.success) {
+      result.error.errors.forEach((issue) => {
+        toast.error(issue.message);
+      });
+      return;
+    }
+    redirect(`/admin/products/search?search=${result.data.search}`);
+  };
+
   return (
-    <form action="" className="flex items-center">
+    <form action={handleSearchForm} className="flex items-center">
       <input
         type="text"
         placeholder="Buscar Producto"
         className="p-2 placeholder-gray-400 w-full"
         name="search"
       />
-      <input type="submit" className="bg-indigo-600 p-2 uppercase text-white cursor-pointer" value={'Buscar'}/>
+      <input
+        type="submit"
+        className="bg-indigo-600 p-2 uppercase text-white cursor-pointer"
+        value={"Buscar"}
+      />
     </form>
   );
 }
