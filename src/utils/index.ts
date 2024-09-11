6320e94e7a4ce80bdc4cbdced1f83b39f7b9bd8f
Dieguidev <diegogaraycullas@gmail.com>
2024-09-10 22:20:56 -0500

    Como mostrar las imágenes si vienen de Cloudinary

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,17 @@
+
+
 export function formatCurrency( amount: number){
   return new Intl.NumberFormat('es-PE', {
     style: 'currency',
     currency: 'PEN',
   }).format(amount);
+}
+
+export function getImagePath(imagePath: string){
+  const cloudinaryBaseUrl = 'https://res.cloudinary.com';
+  if(imagePath.startsWith(cloudinaryBaseUrl)){
+    return imagePath;
+  } else {
+    return `/products/${imagePath}.jpg`;
+  }
 }
