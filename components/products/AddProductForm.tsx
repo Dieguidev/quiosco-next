520eae0e5d509caa28a7fece5053cfc013455eb2
Dieguidev <diegogaraycullas@gmail.com>
2024-09-10 20:12:09 -0500

    Refactorizar página de administración de productos para incluir formulario de creación de productos
    Se refactoriza para utilizar componente del servidor en un componente de cliente

--- components/products/AddProductForm.tsx
+++ components/products/AddProductForm.tsx
@@ -1,10 +1,19 @@
-import ProductForm from "./ProductForm";
+"use client";
 
-export default function AddProductForm() {
+export default function AddProductForm({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const handleSubmit = async (formData: FormData) => {
+    console.log("desde hadlesubmit");
+  };
+
   return (
     <div className="bg-white mt-10 px-5 py-10 rounded-md shadow-md max-w-3xl mx-auto">
-      <form className="space-y-5">
-        <ProductForm />
+      <form className="space-y-5" action={handleSubmit}>
+        {children}
+
         <input
           type="submit"
           className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
