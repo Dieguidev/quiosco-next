89db87d0ffca351730686f61b752887c2ed25a1c
Dieguidev <diegogaraycullas@gmail.com>
2024-09-10 20:18:20 -0500

    Refactorizar formulario de creación de productos y validar datos ingresados
    se crea ProductSchema para validar datos

--- components/products/AddProductForm.tsx
+++ components/products/AddProductForm.tsx
@@ -1,12 +1,27 @@
 "use client";
 
+import { ProductSchema } from "@/src/schema";
+import { toast } from "react-toastify";
+
 export default function AddProductForm({
   children,
 }: {
   children: React.ReactNode;
 }) {
   const handleSubmit = async (formData: FormData) => {
-    console.log("desde hadlesubmit");
+    const data = {
+      name: formData.get("name") as string,
+      price: Number(formData.get("price")),
+      categoryId: Number(formData.get("categoryId")),
+    };
+    const result = ProductSchema.safeParse(data);
+    if (!result.success) {
+      result.error.errors.forEach(error => {
+        toast.error(error.message)
+      })
+      return;
+    }
+
   };
 
   return (
